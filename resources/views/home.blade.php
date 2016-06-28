0786081466e53bb3dd2cc8d03419ac44871c3ca9
Nenad Ticaric <nticaric@gmail.com>
2016-06-28 11:11:51 +0200

    new layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,58 +2,39 @@
 
 @section('content')
   @foreach($news as $entry)
-    <div class="col-md-4 grid-item">
-      <div class="boxed push-down-45">
+    <article class="queue-article">
+        <div class="content-width">
+            <header class="queue-article-obsession-header">
+                <a href="" class="queue-article-obsession-name">The Future of Hong Kong</a>
+                <a href="">See all</a></i>
+            </header>
 
-          <div class="meta">
-            <a href="{{$entry->link}}">
-              <img class="wp-post-image" src="{{$entry->image}}" alt="Blog image" width="748" height="324">
-            </a>
-            <div class="meta__container">
-              <div class="row">
-                <div class="col-xs-12  col-sm-12">
-                  <div class="meta__info">
-                    <span class="meta__author"><img src="images/dummy/about-5.jpg" alt="Meta avatar" width="30" height="30"> <a href="#">{{$entry->author}}</a> in <a href="#">{{$entry->category}}</a> </span>
-                    <span class="meta__date"><span class="fa fa-calendar"></span> {{$entry->updated}}</span>
-                  </div>
+            <h1 class="queue-article-title"><a href="{{$entry->link}}">{{$entry->title}}</a></h1>
+
+            <figure class="queue-article-hero">
+                <picture>
+                    <img src="{{$entry->image}}" />
+                </picture>
+            </figure>
+            <div class="queue-article-content">
+                <p class="queue-article-summary">{{$entry->summary}}</p>
+
+                <div class="queue-article-meta">
+
+                    <div class="item-obsession">
+                        <a href="">{{$entry->category}}</a>
+                    </div>
+
+                    <div class="byline">
+                        <a href="" class="author-name">{{$entry->author}}</a>
+                    </div>
+
+                    <div class="timestamp" title="">{{$entry->updated}}</div>
                 </div>
-              </div>
             </div>
-          </div>
 
-
-          <div class="row">
-              <div class="col-xs-10  col-xs-offset-1">
-
-                  <div class="post-content--front-page">
-                      <h2 class="front-page-title">
-                        <a href="{{$entry->link}}">{{$entry->title}}</a>
-                      </h2>
-                      <h3>{{$entry->summary}}</h3>
-                  </div>
-
-                  <a href="{{$entry->link}}">
-                      <div class="read-more">
-                          Continue reading
-                          <div class="read-more__arrow">
-                              <span class="fa fa-chevron-right"></span>
-                          </div>
-                      </div>
-                  </a>
-              </div>
-          </div>
-      </div>
-    </div>
+        </div>
+    </article>
   @endforeach
 
-  <script type="text/javascript">
-      $( window ).load( function() {
-        var elem = document.querySelector('.grid');
-        var msnry = new Masonry( elem, {
-          // options
-          itemSelector: '.grid-item',
-        });
-      });
-
-  </script>
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,84 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html dir="ltr" lang="en-US">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <link rel="shortcut icon" href="images/favicon.png">
-    <title>TNT Readable News</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
+    <title>TNT News</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/stylesheets/363f9277.main.css" />
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js"></script>
-
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-      <![endif]-->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic">
+    <link rel="stylesheet" href="/css/main.css" />
 
 </head>
 
-<body>
-    <header class="header push-down-45">
-        <div class="container">
-            <div class="logo  pull-left">
-                <a href="/">
-                    <img src="/images/logo.png" alt="Logo" width="352" height="140">
-                </a>
-            </div>
-
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#readable-navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-            <nav class="navbar  navbar-default" role="navigation">
-
-                <div class="collapse  navbar-collapse" id="readable-navbar-collapse">
-                    <ul class="navigation">
-                        <li @if(app('request')->segment(1) == 'jutarnji' || app('request')->segment(1) == '') class="active" @endif>
-                            <a href="/" class="dropdown-toggle" data-toggle="dropdown">Jutarnji.hr</a>
-                        </li>
-                        <li @if(app('request')->segment(1) == 'index.hr' ) class="active" @endif>
-                            <a href="/index.hr" class="dropdown-toggle" data-toggle="dropdown">Index.hr</a>
-                        </li>
-                        <li @if(app('request')->segment(1) == 'net.hr' ) class="active" @endif>
-                            <a href="/net.hr" class="dropdown-toggle" data-toggle="dropdown">Net.hr</a>
-                        </li>
-                        <li @if(app('request')->segment(1) == 'vecernji.hr' ) class="active" @endif>
-                            <a href="/vecernji.hr" class="dropdown-toggle" data-toggle="dropdown">Vecernji.hr</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+<body class="home">
+    <header id="header" class="">
+        <div class="left">
+            <a href="/">jutarnji.hr</a>
+            <a href="/index.hr">index.hr</a>
+            <a href="/net.hr">net.hr</a>
+            <a href="/vecernji.hr">vecernji.hr</a>
         </div>
     </header>
-    
-    <div class="multi-columns">
-        <div class="container">
-            <div class="grid row">
-                @yield('content')
+
+    <div id="container">
+        <div id="content">
+            <div id="content-articles">
+                <div id="queue">
+                    @yield('content')
+                </div>
             </div>
         </div>
     </div>
-    
-    <footer class="copyrights">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12  col-sm-6">
-                    <a href="http://tntstudio.us" target="_blank">TNT Studio</a> News Reader © Copyright {{date('Y')}}.
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script src="/js/main.js"></script>
-
 </body>
 
 </html>
