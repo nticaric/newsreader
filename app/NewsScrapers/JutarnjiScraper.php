a172ae88f0789c09d66f35d26143a4ff50f0da6e
Nenad Ticaric <nticaric@gmail.com>
2016-06-28 10:37:07 +0200

    jutarnji

--- app/NewsScrapers/JutarnjiScraper.php
+++ app/NewsScrapers/JutarnjiScraper.php
@@ -1,92 +1,93 @@
 <?php namespace App\NewsScrapers;
 
+use DateTime;
+use Exception;
 use Goutte\Client;
 use SimpleXMLElement;
 use stdClass;
-use DateTime;
-use Exception;
 
-class JutarnjiScraper {
+class JutarnjiScraper
+{
 
     public function parseHomePage()
     {
         $client = new Client();
 
-        $url = "http://www.jutarnji.hr/rss";
+        $url     = "http://www.jutarnji.hr/rss?type=all";
         $crawler = $client->request('GET', $url);
 
-        $news = [];
+        $news    = [];
         $entries = new SimpleXMLElement(file_get_contents($url));
 
-        foreach ($entries as $entry) {
-            if($entry->link[2]) {
-                $n = new stdClass;
-                $n->link = $this->prepareLink( (string)$entry->link[0]['href'] );
-                $n->image = (string)$entry->link[2]['href'];
-                $n->category = (string)$entry->category[0]['term'];
-                $n->title = (string) $entry->title;
-                $n->summary = (string) $entry->summary;
-                $n->author  = $entry->author->name;
-                $n->updated  = (new DateTime($entry->updated))->format("d M G:i");
-                $news[] = $n;
-            }
-
+        foreach ($entries->entry as $entry) {
+            $n           = new stdClass;
+            $n->link     = $this->prepareLink((string) $entry->link[0]['href']);
+            $n->image    = $this->getImageFromEntry($entry);
+            $n->category = (string) $entry->category[0]['term'];
+            $n->title    = (string) $entry->title;
+            $n->summary  = (string) $entry->summary;
+            $n->author   = $entry->author->name;
+            $n->updated  = (new DateTime($entry->updated))->format("d M G:i");
+            $news[]      = $n;
         }
         return $news;
     }
 
     public function getArticle($slug, $id)
     {
-        $url = "http://www.jutarnji.hr/".$slug."/".$id;
-        $client = new Client();
+        $url     = "http://www.jutarnji.hr/" . $slug . "/" . $id;
+        $client  = new Client();
         $crawler = $client->request('GET', $url);
 
-        $text = $this->prepareLink($crawler->filterXPath('//*[@class="dr_article"]')->html());
+        $text  = $this->prepareLink($crawler->filterXPath('//*[@class="dr_article"]')->html());
         $image = $crawler->filterXPath('//*[@id="foto"]/ul/li/div[1]/img')->attr('src');
-        
+
         return [$text, $image];
     }
 
     public function getGallery($id)
     {
-        $url = "http://www.jutarnji.hr/".$id;
-        $client = new Client();
+        $url     = "http://www.jutarnji.hr/" . $id;
+        $client  = new Client();
         $crawler = $client->request('GET', $url);
-        $newUrl = $crawler->filterXPath("//meta[@property='og:url']")->attr('content');
+        $newUrl  = $crawler->filterXPath("//meta[@property='og:url']")->attr('content');
 
         $newUrl = str_replace("?foto=1", "", $newUrl);
-        $page = 1;
+        $page   = 1;
         $images = [];
         do {
             $page++;
             try {
-                $image = $crawler->filterXPath('//*[@class="foto"]/a/img')->attr('src');
-                $crawler = $client->request('GET', $newUrl."?foto=$page");
+                $image   = $crawler->filterXPath('//*[@class="foto"]/a/img')->attr('src');
+                $crawler = $client->request('GET', $newUrl . "?foto=$page");
             } catch (Exception $e) {
                 $image = null;
             }
 
-            if($image) $images[] = $image;
+            if ($image) {
+                $images[] = $image;
+            }
+
         } while ($image);
         return $images;
     }
 
     public function getImagesForDomIDizajn($slug, $id)
     {
-        $url = "http://www.jutarnji.hr/".$slug."/".$id;
-        $client = new Client();
+        $url     = "http://www.jutarnji.hr/" . $slug . "/" . $id;
+        $client  = new Client();
         $crawler = $client->request('GET', $url);
 
         $newUrl = $crawler->filterXPath("//meta[@property='og:url']")->attr('content');
 
-        $crawler = $client->request('GET', $newUrl);
+        $crawler     = $client->request('GET', $newUrl);
         $galleryLink = $crawler->filterXPath('//*[@class="single-text-gallerylink button"]')->attr('href');
-        
+
         $crawler = $client->request('GET', $galleryLink);
 
-        $postID = $crawler->filterXPath('//*[@name="comment_post_ID"]')->attr('value');
-        $url = "http://domidizajn.jutarnji.hr/wp-admin/admin-ajax.php?action=dd_gallery_photos&postid=".$postID;
-        $json = file_get_contents($url);
+        $postID        = $crawler->filterXPath('//*[@name="comment_post_ID"]')->attr('value');
+        $url           = "http://domidizajn.jutarnji.hr/wp-admin/admin-ajax.php?action=dd_gallery_photos&postid=" . $postID;
+        $json          = file_get_contents($url);
         $galleryThumbs = json_decode($json);
 
         foreach ($galleryThumbs->photos as $thumb) {
@@ -95,26 +96,27 @@ class JutarnjiScraper {
         return $images;
     }
 
-    public function getArticleType($slug, $id) 
+    public function getArticleType($slug, $id)
     {
-        $url = "http://www.jutarnji.hr/".$slug."/".$id;
-        $client = new Client();
+        $url     = "http://www.jutarnji.hr/" . $slug . "/" . $id;
+        $client  = new Client();
         $crawler = $client->request('GET', $url);
 
         $newUrl = $crawler->filterXPath("//meta[@property='og:url']")->attr('content');
 
-        if (strpos($newUrl,'?foto=1') !== false) {
+        if (strpos($newUrl, '?foto=1') !== false) {
             return 'gallery';
         }
 
-        if(strpos($newUrl, 'domidizajn.jutarnji.hr') !== false) {
+        if (strpos($newUrl, 'domidizajn.jutarnji.hr') !== false) {
             return 'domidizajn';
         }
 
         return 'regular';
     }
 
-    public function prepareLink($text) {
+    public function prepareLink($text)
+    {
         $text = str_replace("http://www.jutarnji.hr/", '/jutarnji/', $text);
         $text = str_replace('src="/jutarnji', 'src="http://jutarnji.hr', $text);
 
@@ -122,11 +124,23 @@ class JutarnjiScraper {
         return $text;
     }
 
-    public function customTrim($str) {
+    public function customTrim($str)
+    {
         $str = str_replace("&nbsp;", '', $str);
         $str = str_replace("&#160;", "", $str);
         $str = trim($str);
         $str = trim($str, "\xC2\xA0");
         return $str;
+    }
+
+    public function getImageFromEntry($entry)
+    {
+        $imageLink = "";
+        foreach ($entry->link as $link) {
+            if (strpos($link['type'], 'image') !== false) {
+                $imageLink = (string) $link['href'];
+            }
+        }
+        return $imageLink;
     }
 }
