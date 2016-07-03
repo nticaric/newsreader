f41861767e8c3e06524043ec8cb5c6a74cb7d84b
Nenad Ticaric <nticaric@gmail.com>
2016-07-03 23:29:01 +0200

    fixed top

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
 </head>
 
 <body class="home">
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container-fluid">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
