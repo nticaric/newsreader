3e18ec3379498b8a74ded45ad8d1124f7ff45863
Nenad Ticaric <nticaric@gmail.com>
2017-12-05 18:45:20 +0100

    remove header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,9 @@
   <div class="container">
   @foreach($news as $entry)
     <article class="queue-article">
-        <header class="queue-article-obsession-header">
-            <a href="" class="queue-article-obsession-name">The Future of Hong Kong</a>
-            <a href="">See all</a></i>
-        </header>
 
         <h1 class="queue-article-title"><a href="{{$entry->link}}">{{$entry->title}}</a></h1>
-        
+
         <div class="row">
             <div class="col-md-3">
                 <figure class="queue-article-hero">
@@ -19,7 +15,7 @@
                         <img src="{{$entry->image}}" />
                       </a>
                     </picture>
-                </figure>                
+                </figure>
             </div>
             <div class="col-md-9">
                 <div class="queue-article-content">
